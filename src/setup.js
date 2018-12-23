f130ce701f7715c35ea8af0abb9fa50215159f37
Valen. H <alternativexxxy@gmail.com>
2018-12-23 16:44:12 +0200

    Update

--- src/setup.js
+++ src/setup.js
@@ -12,7 +12,7 @@ function check() {
 	fs.ensureDirSync(path.join(config.localpath, "/CSS"));
 
 	if (!fs.existsSync(path.join(config.localpath, "/JS5/polyfill.min.js"))) {
-		fs.copyFileSync(path.join(__dirname, "node_modules/babel-polyfill/dist/polyfill.min.js"), path.join(config.localpath, "/JS5/polyfill.min.js"), fs.constants.COPYFILE_EXCL | fs.constants.COPYFILE_FICLONE);
+		fs.copyFileSync("node_modules/babel-polyfill/dist/polyfill.min.js", path.join(config.localpath, "/JS5/polyfill.min.js"), fs.constants.COPYFILE_EXCL | fs.constants.COPYFILE_FICLONE);
 		console.info(chalk`{gray Created file: ${config.localpath}/JS5/polyfill.min.js}`);
 	}
 } //check
